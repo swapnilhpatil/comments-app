8ba9cd98cae093600aaf7565686f9fdbe5aeb3c8
Swapnil <swapnilhpatil9999@gmail.com>
2024-05-29 23:07:40 +0530

    removed unsed comments

--- src/App.js
+++ src/App.js
@@ -1,50 +1,42 @@
-// import logo from './logo.svg';
-import './App.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import Login from './components/login';
-import Header from './components/header';
-import CommentsPage from './components/comments';
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Login from "./components/login";
+import Header from "./components/header";
+import CommentsPage from "./components/comments";
+import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      {/* <Header/> */}
-      {/* <Login/> */}
       <Router>
-      <div>
-        {isLoggedIn && <Header setIsLoggedIn={setIsLoggedIn}/>}
-        <Switch>
-          <Route exact path="/">
-            {isLoggedIn ? <Redirect to="/comments" /> : <Login setIsLoggedIn={setIsLoggedIn} />}
-          </Route>
-          <Route path="/comments">
-            {isLoggedIn ? <CommentsPage /> : <Redirect to="/" />}
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+        <div>
+          {isLoggedIn && <Header setIsLoggedIn={setIsLoggedIn} />}
+          <Switch>
+            <Route exact path="/">
+              {isLoggedIn ? (
+                <Redirect to="/comments" />
+              ) : (
+                <Login setIsLoggedIn={setIsLoggedIn} />
+              )}
+            </Route>
+            <Route path="/comments">
+              {isLoggedIn ? <CommentsPage /> : <Redirect to="/" />}
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
